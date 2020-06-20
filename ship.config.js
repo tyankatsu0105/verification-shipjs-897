5aedf99a64b1562368355e5ba785884df427d2ff
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-06-20 10:03:02 +0900

    build: add shipjs

---
+++ ship.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  publishCommand: ({ defaultCommand }) => `${defaultCommand} --access public`,
+  buildCommand: () => null,
+  afterPublish({ exec }) {
+    exec("git config --global user.email frips.ryilsufupe+dev@gmail.com");
+    exec("git config --global user.name tyankatsu");
+
+    exec("git checkout master");
+    exec("git merge develop");
+    exec("git push origin master");
+  },
+};
